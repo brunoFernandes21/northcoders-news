b3db05852533b948ac613372a0a0d451d7381f1c
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-06-26 14:59:22 +0100

    remove all the files added and changes made to main

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require("express")
 const { getAllTopics } = require("./controllers/topics.controller")
 
-
 const app = express()
 
 app.get("/api/topics", getAllTopics)
-
 
 app.all(("*"),(request, response) => {
     response.status(404).send({msg: "Not Found"})
