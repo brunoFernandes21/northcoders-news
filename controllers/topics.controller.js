fbe25fb88efcf3b97efb860e35fefbacda6809b9
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-06-26 13:22:54 +0100

    delete files pushed by accident

--- controllers/topics.controller.js
+++
@@ -1,7 +0,0 @@
-const { selectAllTopics } = require("../models/topics.model")
-
-exports.getAllTopics = (request, response, next) => {
-    selectAllTopics().then(( topics ) => {
-        response.status(200).send({ topics })
-    })
-}
--- models/topics.model.js
+++
@@ -1,7 +0,0 @@
-const db = require("../db/connection")
-exports.selectAllTopics = () => {
-    const queryString = "SELECT * FROM topics";
-    return db.query(queryString).then(({ rows }) => {
-        return rows
-    })
-}
