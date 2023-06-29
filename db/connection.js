ca6cb69f97a592b2bc8c5998d7af543d98cae07b
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-06-29 15:33:21 +0100

    update connection.js

--- db/connection.js
+++ db/connection.js
@@ -6,7 +6,6 @@ require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
 
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
@@ -19,4 +18,5 @@ if (ENV === 'production') {
 }
 
 
-module.exports = new Pool();
+
+module.exports = new Pool(config);
