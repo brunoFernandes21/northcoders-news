1a632bc6d71791090d72c8baab1e27f3bbb69fc9
Bruno Fernandes <47353649+brunoFernandes21@users.noreply.github.com>
2023-07-04 19:17:13 +0100

    reversed unwanted changes

--- models/articles.model.js
+++ models/articles.model.js
@@ -10,7 +10,7 @@ exports.selectAllArticles = () => {
 };
 
 exports.selectArticleById = ( id ) => {
-    const queryString = "SELECT * FROM articles WHERE article_id = $1";
+    const queryString = "SELECT * FROM articles  WHERE article_id = $1";
     return db.query(queryString, [id]).then(( { rows}) => {
         if(rows.length === 0) {
             return Promise.reject({status: 404, msg: "Not found"})
